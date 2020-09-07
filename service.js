4c619bec940895e65be2234e6de2bc59052a9b85
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2020-09-07 17:30:06 +0800

    added working code

--- service.js
+++ service.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3009;
 const app = express();
 
 app.use(logger("dev"));
@@ -10,11 +10,13 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", { useNewUrlParser: true, useFindAndModify: false });
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds231725.mlab.com:31725/heroku_sqc6mlww", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds231725.mlab.com:31725/heroku_sqc6mlww", {
+  //useNewUrlParser: true,
+  //useFindAndModify: false
+//});
+
 
 app.use(require("./routes/api.js"));
 
